03d50dc4a89049fc9165d8477feee7be979d6f82
Saurav <sauravkumar00108@gmail.com>
2024-10-05 00:50:31 +0530

    Add FlipedInverted.java to print a flipped inverted pattern

--- pattern/FlipedInverted.java
+++ pattern/FlipedInverted.java
@@ -3,6 +3,24 @@ import java.util.*;
 
 public class FlipedInverted{
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entwr the number");
+        int num1 = sc.nextInt();
+
+        System.out.println("You entered " + num1);
+
+        for(int i = 1; i<= num1; i++)
+        {
+            for(int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= num1-i+1; k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
