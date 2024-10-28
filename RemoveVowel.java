29ed9c6e676402a201920ae74295d6e1866cc1a0
Saurav <sauravkumar00108@gmail.com>
2024-10-28 16:20:51 +0530

    Refactor RemoveVowel class to improve readability and efficiency

--- RemoveVowel.java
+++ RemoveVowel.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
-public class RemoveVowel{
-    public static void main(String[] args){
+public class RemoveVowel {
+    public static void main(String[] args) {
         try {
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a string to remover vowel in it");
-        String str = sc.nextLine();
+            System.out.println("Enter a string to remover vowel in it");
+            String str = sc.nextLine();
 
-        StringBuffer sbuf = new StringBuffer(str);
+            StringBuffer sbuf = new StringBuffer(str);
 
-        for(int i = 0 ; i < sbuf.length(); i++)
-        {
-            char ch = sbuf.charAt(i);
-            if(ch == 'a' ||ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u' ||ch == 'A' ||ch == 'E' ||ch == 'I' ||ch == 'O' ||ch == 'U')
-            {
-                sbuf.deleteCharAt(i);
-                i--;
+            for (int i = 0; i < sbuf.length(); i++) {
+                char ch = sbuf.charAt(i);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+                        || ch == 'O' || ch == 'U') {
+                    sbuf.deleteCharAt(i);
+                    i--;
+                }
             }
-        }
 
-        System.out.println("String without buffer " + sbuf.toString());
+            System.out.println("String without buffer " + sbuf.toString());
         } catch (Exception e) {
             System.out.println("Error=-=-=" + e);
 
